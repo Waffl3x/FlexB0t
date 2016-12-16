f7c62b93767694424257057870ec421a75538e9e
Alex <wolf.alex30@gmail.com>
2016-12-16 04:45:41 -0500

    added !suggestions command
    also converted tabs to spaces causing everything to show changes, I dont
    know why there were tabs in the file to begin with

--- plugins/myFirstPlugin.py
+++ plugins/myFirstPlugin.py
@@ -18,82 +18,96 @@ privmsgcmdTrigger = {}
 #!flame
 #!flame target
 def flameUser(user, channel, message, arguments):
-	if arguments == []:
-		target = user
-	else:
-		target = arguments[0]
+    if arguments == []:
+        target = user
+    else:
+        target = arguments[0]
 
-	phrase = [
-		'{0} is a retard',
-		'{0} is a potato'
+    phrase = [
+        '{0} is a retard',
+        '{0} is a potato'
 
-	]
-	random.shuffle(phrase)
+    ]
+    random.shuffle(phrase)
 
-	return phrase[0].format(target)
+    return phrase[0].format(target)
 
 privmsgcmdTrigger['!flame'] = flameUser
 
 #!compliment target
 def compliment(user, channel, message, arguments):
-	target = arguments[0]
+    target = arguments[0]
 
-	phrase = [
-		'{0} is not a retard',
-		'{0} is not a potato',
-		'{0} is very nice'
+    phrase = [
+        '{0} is not a retard',
+        '{0} is not a potato',
+        '{0} is very nice'
 
-	]
-	random.shuffle(phrase)
+    ]
+    random.shuffle(phrase)
 
-	return phrase[0].format(target)
+    return phrase[0].format(target)
 
 privmsgcmdTrigger['!compliment'] = compliment
 
 #!gaydar
 #!gaydar target
 def gaydar(user, channel, message, arguments):
-	if arguments == []:
-		target = user
-	else:
-		target = arguments[0]
+    if arguments == []:
+        target = user
+    else:
+        target = arguments[0]
 
-	return '{} is {}% KappaPride'.format(target, random.randrange(0,100,1))
+    return '{} is {}% KappaPride'.format(target, random.randrange(0,100,1))
 
 privmsgcmdTrigger['!gaydar'] = gaydar
 
 #!startfight fighter1 fighter2
 def startfight(user, channel, message, arguments):
-	encounter = [
-		'{0} punches {1}, who falls over. {1} now has a sore ass.',
-		'{0} hits {1} right in the face. {1} goes flying!',
-		'{0} hits {1} with a fan. {1} flies a few feet off to the side.',
-		'{0} blows in {1}\'s general direction. {1} falls over!'
-	
-	]
-	target = arguments[0:2]
-	random.shuffle(target)
-	random.shuffle(encounter)
-	return encounter[0].format(target[0], target[1])
+    encounter = [
+        '{0} punches {1}, who falls over. {1} now has a sore ass.',
+        '{0} hits {1} right in the face. {1} goes flying!',
+        '{0} hits {1} with a fan. {1} flies a few feet off to the side.',
+        '{0} blows in {1}\'s general direction. {1} falls over!'
+    
+    ]
+    target = arguments[0:2]
+    random.shuffle(target)
+    random.shuffle(encounter)
+    return encounter[0].format(target[0], target[1])
 
 privmsgcmdTrigger['!startfight'] = startfight
 
 #!fight target
 def fight(user, channel, message, arguments):
-	encounter = [
-		'{0} punches {1}, who falls over. {1} now has a sore ass.',
-		'{0} hits {1} right in the face. {1} goes flying!',
-		'{0} hits {1} with a fan. {1} flies a few feet off to the side.',
-		'{0} blows in {1}\'s general direction. {1} falls over!'
+    encounter = [
+        '{0} punches {1}, who falls over. {1} now has a sore ass.',
+        '{0} hits {1} right in the face. {1} goes flying!',
+        '{0} hits {1} with a fan. {1} flies a few feet off to the side.',
+        '{0} blows in {1}\'s general direction. {1} falls over!'
 
-	]
-	target = [arguments[0], user]
-	random.shuffle(target)
-	random.shuffle(encounter)
-	return encounter[0].format(target[0], target[1])
+    ]
+    target = [arguments[0], user]
+    random.shuffle(target)
+    random.shuffle(encounter)
+    return encounter[0].format(target[0], target[1])
 
 privmsgcmdTrigger['!fight'] = fight
 
+#temporary list until I add a file or something
+suggestionBlacklist = []
+#!suggest suggestion
+#save a suggestion for a command to a text file
+def suggest(user, channel, message, arguments):
+    if not(user in suggestionBlacklist):
+        if (len(message) < 100):
+            with open('suggestions.txt', 'a') as t:
+                t.write('{0} in {1} suggested: {2}\n'.format(user, channel, message[9:]))
+            return 'Thank you for the suggestion!'
+        else:
+            return 'Suggestion was too long, please enter a shorter one.'
+
+privmsgcmdTrigger['!suggest'] = suggest
 
 
 triggerManifest = set(privmsgcmdTrigger.keys())
